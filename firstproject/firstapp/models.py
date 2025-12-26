c45f3a0029a5612e5c7f947525869191d00ec65a
mukhtar <wisemuck@gmail.com>
2025-12-26 16:27:03 +0300

    will start from mysql next time

--- firstproject/firstapp/models.py
+++ firstproject/firstapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.BigIntegerField()
+
+class Reservation(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    guest_count = models.IntegerField()
+    comments = models.CharField(max_length=255)
